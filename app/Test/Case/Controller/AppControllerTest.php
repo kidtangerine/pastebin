3385c8647ff404ca479ec6400829dcbf4ac0dbbd
brad <kidtangerine@gmail.com>
2014-08-17 06:50:10 +0000

    Adding first application test

---
+++ app/Test/Case/Controller/AppControllerTest.php
@@ -0,0 +1,18 @@
+<?php
+App::uses('AppController','Controller');
+
+ class AppControllerTest extends ControllerTestCase{ 
+  
+     public function testHasSessionComponent() {
+     
+        $Controller = $this->generate('App');
+        $this->assertArrayHasKey('Session', get_object_vars($Controller));
+      
+     
+     
+     
+        }
+     }
+ 
+ 
+ ?>
